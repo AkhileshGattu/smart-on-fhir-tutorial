1a8b9c1bc83c5457f8e94e31c7d306ea3dc5bb5c
AkhileshGattu <aghgc@mail.umkc.edu>
2017-04-15 23:53:13 -0500

    Added Immunization Methods

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -10,14 +10,14 @@
     function onReady(smart)  {
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
-        //console.log(smart);
-        //console.log(patient);
+//         console.log(smart);
+//         console.log(patient);
         var pt = patient.read();
-        console.log(pt);
-        //console.log(patient.identifier);
-        //console.log(patient.telecom);
-        //console.log(patient.address);
-        //console.log(smart.patient.api);
+//         console.log(pt);
+//         console.log(patient.identifier);
+//         console.log(patient.telecom);
+//         console.log(patient.address);
+//         console.log(smart.patient.api);
         //console.log("FetchALL: " + smart.fetchAll({type: 'patient'}));
         var obv = smart.patient.api.fetchAll({
                     type: 'Observation',
@@ -35,7 +35,9 @@
         var imm = smart.patient.api.fetchAll({
           type: 'Immunization',
           query: {
-            notGiven: 'False'
+            code: {
+              $or: ['http://loinc.org|60494-2', 'http://loinc.org|30940-1']
+            }
           }
         });
         
@@ -45,18 +47,19 @@
 
         $.when(pt, obv, imm).done(function(patient, obv, imm) {
           var byCodes = smart.byCodes(obv, 'code');
+          //Immunization code
+          var immByCodes = smart.byCodes(imm, 'code');
           var gender = patient.gender;
           var dob = new Date(patient.birthDate);
           var day = dob.getDate();
           var monthIndex = dob.getMonth() + 1;
           var year = dob.getFullYear();
-          console.log("After Imm before notgiven");
-          console.log(imm.notGiven);
           //Adding newly
+          console.log(imm.notGiven);
           if(typeof patient.identifier[0] != 'undefined'){
             var identifier = patient.identifier[0].value;
           }
-          console.log(patient.identifier);
+          //console.log(patient.identifier);
           //var identifier = patient.identifier;
           var address = patient.address;
           var telecom = patient.telecom;
@@ -75,6 +78,10 @@
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
+          
+          //Immunization Code
+          var influenza = getInfluenza(byCodes('60494-2'), 'LL956-4');
+          var mmr = byCodes('30940-1');
 
           var p = defaultPatient();
           p.birthdate = dobStr;
@@ -143,6 +150,21 @@
     });
 
     return getQuantityValueAndUnit(formattedBPObservations[0]);
+  }
+  
+  //Influenza Immunization Method
+  function getInfluenza(ImmObservations, typeOfImmunization){
+    var formattedImmObservations = [];
+    ImmObservations.forEach(function(observation){
+      var IMO = observation.find(function(coding){
+        return coding.code == typeOfImmunization;
+      });
+      if(IMO){
+        observation.doseQuantity = IMO.doseQuantity;
+        formattedImmObservations.push(observation);
+      }
+    });
+    return getQuantityValueAndUnit(formattedImmObservations[0]);
   }
 
   function isLeapYear(year) {
